7f29588eab186eba14a6b79caada31544c3ed8f1
glitchFling <glitchfling378@gmail.com>
2026-04-08 10:28:49 -0500

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    MessageBox(NULL, "GUI made with me", "Banana App", MB_OK | MB_ICONINFORMATION);
+    // TEXT() ensures strings are compatible with both Unicode and ANSI builds
+    MessageBox(NULL, TEXT("GUI made with me"), TEXT("Banana App"), MB_OK | MB_ICONINFORMATION);
     return 0;
 }
